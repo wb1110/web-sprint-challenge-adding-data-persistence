5b0369c7c9cb9104efed344b6fb31b66b8f2a08b
wb1110 <strengthcoachwb@gmail.com>
2022-01-21 10:25:07 -0600

    done

--- api/task/model.js
+++ api/task/model.js
@@ -3,8 +3,15 @@ const db = require('../../data/dbConfig')
 
 async function get () {
   const rows = await db('tasks as t')
-  .leftJoin('projects as p','t.task_id', 'p.project_id')
-  .select('t.task_id', 't.task_description', 't.task_notes', 't.task_completed', 'p.project_name', 'p.project_description');
+  .leftJoin('projects as p','t.project_id', '=', 'p.project_id')
+  .select(
+    't.task_id', 
+    't.task_description', 
+    't.task_notes', 
+    't.task_completed', 
+    'p.project_name', 
+    'p.project_description'
+    )
 
   const tasks = []
 
