03b7ec5b831c8cbe8de4e83072cdbea046f21bb9
wb1110 <strengthcoachwb@gmail.com>
2022-01-20 22:50:55 -0600

    tried fixing model

--- api/project/model.js
+++ api/project/model.js
@@ -6,6 +6,13 @@ async function get () {
   // if (projects.project_completed === 0) {
   //   projects.project_completed = 'false'
   // }
+  // projects.forEach(project => {
+  //   if (!project.project_completed) {
+  //     project.project_completed === 'false'
+  //     } else {
+  //       project.project_completed === 'true'
+  //     }
+  //   })
   return projects;
 }
 
