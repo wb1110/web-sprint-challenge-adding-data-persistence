e307c886268192ef5cc20e325eb7a997bb00ad0d
wb1110 <strengthcoachwb@gmail.com>
2022-01-21 09:51:58 -0600

    conversion problem solved

--- api/project/model.js
+++ api/project/model.js
@@ -2,15 +2,39 @@
 const db = require('../../data/dbConfig')
 
 async function get () {
-  const rows = await db('projects as p');
-  return rows;
+
+  const rows = await db('projects as p')
+
+  const projects = []
+
+  rows.forEach(row => {
+    if (row) {
+      projects.push({
+        "project_id": row.project_id,
+        "project_name": row.project_name,
+        "project_description": row.project_description,
+        "project_completed": row.project_completed < 1 ? false : true,
+      })
+    }
+  })
+
+  return projects;
 }
 
 function create (project) {
   return db('projects as p').insert(project)
   .then(([project_id]) => {
     return db('projects').where('project_id', project_id).first()
-  });
+  })
+  .then(row => {
+    const conversion = {
+      "project_id": row.project_id,
+      "project_name": row.project_name,
+      "project_description": row.project_description,
+      "project_completed": row.project_completed < 1 ? false : true,
+    }
+    return conversion
+  })
 }
 
 module.exports = {
--- api/task/model.js
+++ api/task/model.js
@@ -2,7 +2,9 @@
 const db = require('../../data/dbConfig')
 
 async function get () {
-  const rows = await db('tasks');
+  const rows = await db('tasks as t')
+  .leftJoin('projects as p','t.task_id', 'p.project_id')
+  .select('t.task_id', 't.task_description', 't.task_notes', 't.task_completed', 'p.project_name', 'p.project_description');
   return rows;
 }
 
