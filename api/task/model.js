8b74389d4595abb1fe147463b96073bb04ddb379
wb1110 <strengthcoachwb@gmail.com>
2022-01-21 10:12:55 -0600

    one left

--- api/task/model.js
+++ api/task/model.js
@@ -5,14 +5,40 @@ async function get () {
   const rows = await db('tasks as t')
   .leftJoin('projects as p','t.task_id', 'p.project_id')
   .select('t.task_id', 't.task_description', 't.task_notes', 't.task_completed', 'p.project_name', 'p.project_description');
-  return rows;
+
+  const tasks = []
+
+  rows.forEach(row => {
+    if (row) {
+      tasks.push({
+        "task_id": row.task_id,
+        "task_description": row.task_description,
+        "task_notes": row.task_notes,
+        "task_completed": row.task_completed < 1 ? false : true,
+        "project_name": row.project_name,
+        "project_description": row.project_description,
+      })
+    }
+  })
+
+  return tasks;
 }
 
 function create (task) {
-  return db('tasks').insert(task)
+  return db('tasks as t').insert(task)
   .then(([task_id]) => {
     return db('tasks').where('task_id', task_id).first()
-  });
+  })
+  .then(row => {
+    const conversion = {
+      "task_id": row.task_id,
+      "task_description": row.task_description,
+      "task_notes": row.task_notes,
+      "task_completed": row.task_completed < 1 ? false : true,
+      "project_id": row.project_id,
+    }
+    return conversion
+  })
 }
 
 module.exports = {
